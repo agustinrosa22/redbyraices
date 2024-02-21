8dfac20c3175cb947928c780ed3ef43c1aa704d6
agustinrosa22 <agustinrosa1234@gmail.com>
2024-02-21 18:48:33 -0300

    login css

--- src/Views/Login/Login.jsx
+++ src/Views/Login/Login.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../Redux/Actions/actions';
 import style from './Login.module.css';
 import { useNavigate } from 'react-router-dom';
+import titulo from '../../Assets/tituloNuevo.png'
 
 const Login = () => {
   const navigate = useNavigate();
@@ -28,31 +29,44 @@ const Login = () => {
 
   return (
     <div className={style.container}>
-      <h2>Login</h2>
+      <div clasname={style.imgContainer}>
+      <img className={style.img} src={titulo} alt="" />
+      </div>
+      <div className={style.card}>
+      <div className={style.content}>
+      <h2 className={style.title}>Ingresar</h2>
       {error && <div className={style.error}>{error}</div>}
-      <form onSubmit={onSubmit}>
-        <div>
+      <form className={style.form} onSubmit={onSubmit}>
+
           <label>Email:</label>
+        <div className={style.formGroup}>
           <input
             type="email"
             name="mail"
             value={mail}
             onChange={onChange}
             required
-          />
+            className={style.inputForm}
+            />
         </div>
-        <div>
-          <label>Password:</label>
+          <label>Contraseña:</label>
+        <div className={style.formGroup}>
           <input
             type="password"
             name="password"
             value={password}
             onChange={onChange}
             required
-          />
+            className={style.inputForm}
+            />
         </div>
-        <button type="submit">Login</button>
+        <button  className={style.buttonSubmit} type="submit">Login</button>
       </form>
+      <p className={style.forgotPassword}>
+                        <a className={style.forgotPasswordLink} href="/recuperar-contraseña">Olvidé mi contraseña</a>
+                    </p>
+       </div>
+       </div>
     </div>
   );
 };
