ef7f494b1f64fb19908ca11164c545e3fca092d1
agustinrosa22 <agustinrosa1234@gmail.com>
2025-02-18 15:13:29 -0300

    acm 80%

--- src/Views/ACM/ACMReport.jsx
+++ src/Views/ACM/ACMReport.jsx
@@ -3,9 +3,15 @@ import style from "./ACMReport.module.css";
 import portada from "../../Assets/Boceto ACM.png";
 import fondoACM from "../../Assets/fondoACM.png";
 import html2pdf from 'html2pdf.js';
+import mapa from '../../Assets/IlustracionDeMapaGoogleMaps.png'
+import logoByraices from '../../Assets/LogoByraicesACM.png'
+import logoInmoup from '../../Assets/LogoInmoupACM.png'
+import logoZopaprop from '../../Assets/LogoZonapropACM.png'
 
 const ACMReport = () => {
   const reportRef = useRef();
+  const [images, setImages] = useState([null, null, null]); // Imágenes de la primera página
+  const [imagesII, setImagesII] = useState([null, null, null]); // Imágenes de la segunda página
 
   const handleDownloadPDF = () => {
     const options = {
@@ -35,6 +41,35 @@ const ACMReport = () => {
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+
+   // Manejo de cambio de imagen para la primera página
+   const handleImageChange = (event, index) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const newImages = [...images];
+        newImages[index] = e.target.result;
+        setImages(newImages);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // Manejo de cambio de imagen para la segunda página
+  const handleImageChangeII = (event, index) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const newImagesII = [...imagesII];
+        newImagesII[index] = e.target.result;
+        setImagesII(newImagesII);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   return (
@@ -127,51 +162,179 @@ similares en el mercado. Analizaremos diversos factores, incluyendo la
 ubicación, el tamaño, las comodidades, el estado de la propiedad y
 otros elementos importantes, con el fin de determinar el valor más
 preciso para tu propiedad.</p>
+
+<img className={style.mapa}  src={mapa} alt="" />
 </div>
 </div>
 
 
         {/* Página 6 - Comparación de Mercado */}
 <div className={style.page}>
+<h2 className={style.title}>Portales de Referencias</h2>
+<div className={style.AllReferencias}>
 
+<div className={style.containerRefencia}>
+<img src={logoByraices} alt="" />
+<h3>Byraices.com</h3>
+</div>
+
+<div className={style.containerRefencia}>
+<img src={logoInmoup} alt="" />
+<h3>Inmoup.com</h3>
+</div>
+
+<div className={style.containerRefencia}>
+<img src={logoZopaprop} alt="" />
+<h3>Zonaprop.com</h3>
+</div>
+</div>
 </div>
 
         {/* Página 7 - Comparación de Mercado */}
 <div className={style.page}>
+<h2 className={style.title}>F.O.D.A</h2>
+
+<div className={style.itemFodaF}>
+
+<h3 className={style.titleFodaF}>FORTALEZAS</h3>
+<p className={style.textFoda}>Muy buena ubicacion de la propiedad. Cercanias a distintos tipos de comercios. Como calle principal se destaca la calle housay de facil salida a accesos rapidos. </p>
+
+</div>
+
+<div className={style.itemFodaO}>
+
+<h3 className={style.titleFodaO}>OPORTUNIDADES</h3>
+<p className={style.textFoda}>  Posibles inversores pueden comprar la propiedad, y haciendos sus distintos arreglos pueden obtener una rentabilidad futura; eso hara que la mirada sea distinta hacia la propiedad.  </p>
+
+</div>
+
+
+<div className={style.itemFodaD}>
+
+<h3 className={style.titleFodaD}>DEBILIDADES</h3>
+<p className={style.textFoda}>Debido a su antiguedad puede que presente algunas falencias, que a simple vista no se pueden apreciar.</p>
+
+</div>
+
+<div className={style.itemFodaA}>
+
+<h3 className={style.titleFodaA}>AMENAZAS</h3>
+<p className={style.textFoda}>no presenta</p>
+
+</div>
+
 
 
 </div>
  
  
         {/* Página 8 - Comparación de Mercado */}
-<div className={style.page}>
+        <div className={style.page}>
+  <h2 className={style.title}>Planilla Comparables</h2>
 
+  <div className={style.containerTable}>
+
+  <table className={style.table}>
+    <thead>
+      <tr>
+        <th>FICHA ESTUDIO</th>
+        <th>PROPIEDAD ESTUDIADA</th>
+        <th>COMP I</th>
+        <th>COMP II</th>
+      </tr>
+    </thead>
+    <tbody>
+      {[
+        "DIRECCIÓN",
+        "METROS CUBIERTOS",
+        "METROS CON ESPACIOS COMUNES",
+        "DORMITORIOS",
+        "ANTIGÜEDAD",
+        "ESTADO DE CONSERVACIÓN",
+        "VALOR DE PUBLICACIÓN"
+
+      ].map((rowTitle, rowIndex) => (
+        <tr key={rowIndex}>
+          <td className={style.rowTitle}>{rowTitle}</td>
+          {[...Array(3)].map((_, colIndex) => (
+            <td key={colIndex} contentEditable={true} className={style.cell}></td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+
+  </div>
 </div>
 
 
-        {/* Página 9 - Comparación de Mercado */}
-<div className={style.page}>
 
-</div>
+  {/* Página 9 - Comparación de Mercado */}
+  <div className={style.page}>
+        <h2 className={style.title}>FOTOGRAFIA COMPARABLE I</h2>
+        <div className={style.containerImagenes}>
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={style.imageBox}
+              onClick={() => document.getElementById(`fileInputI${index}`).click()} // ID único para cada imagen
+            >
+              {image ? (
+                <img src={image} alt={`Foto ${index + 1}`} className={style.image} />
+              ) : (
+                <p>Seleccionar Imagen</p>
+              )}
+              <input
+                type="file"
+                id={`fileInputI${index}`} // ID único para cada input
+                accept="image/*"
+                onChange={(e) => handleImageChange(e, index)}
+                hidden
+              />
+            </div>
+          ))}
+        </div>
+      </div>
 
 
-        {/* Página 10 - Comparación de Mercado */}
-<div className={style.page}>
-
-
-</div>
+ {/* Página 10 - Comparación de Mercado */}
+      <div className={style.page}>
+        <h2 className={style.title}>FOTOGRAFIA COMPARABLE II</h2>
+        <div className={style.containerImagenes}>
+          {imagesII.map((image, index) => (
+            <div
+              key={index}
+              className={style.imageBox}
+              onClick={() => document.getElementById(`fileInputII${index}`).click()} // ID único para cada imagen
+            >
+              {image ? (
+                <img src={image} alt={`Foto ${index + 1}`} className={style.image} />
+              ) : (
+                <p>Seleccionar Imagen</p>
+              )}
+              <input
+                type="file"
+                id={`fileInputII${index}`} // ID único para cada input
+                accept="image/*"
+                onChange={(e) => handleImageChangeII(e, index)}
+                hidden
+              />
+            </div>
+          ))}
+        </div>
+      </div>
 
 
         {/* Página 11 - Comparación de Mercado */}
 <div className={style.page}>
-
+<h2 className={style.title}>RESULTADO  DEL ANALISIS DE MERCADO</h2>
 
 </div>
 
 
         {/* Página 12 - Comparación de Mercado */}
 <div className={style.page}>
-
+<h2 className={style.title}>¿COMO AYUDARTE CON LA COMERCIALIZACIÓN DE TU PROPIEDAD?</h2>
 
 </div>
 
